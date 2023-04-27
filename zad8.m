0c5f7eaf226d0ec94954115362a47107a88cacf7
pkutyl <01168894@pw.edu.pl>
2023-04-27 12:20:36 +0200

    Add 8 plots and 8 simulink

--- zad8.m
+++ zad8.m
@@ -1,5 +1,5 @@
 
-var_plot_1 = out.x1_obj;
+var_plot_1 = out.x1_obj8;
 data = var_plot_1.signals.values;
 time = var_plot_1.time;
 subplot(2,2,1)
@@ -10,13 +10,13 @@ set(gca,'fontsize', 8)
 title("x1")
 % xticks(0:1:10);
     hold on
-var_plot_1_2 = out.x1_obv;
+var_plot_1_2 = out.x1_obv8;
 data = var_plot_1_2.signals.values;
 time = var_plot_1_2.time;
 stairs(time,data,'Color','b','LineStyle','--', 'LineWidth',0.9)
 legend("x1 obiektu","x1 obserwatora","Location", "southeast")
 hold off
-var_plot_2 = out.x2_obj;
+var_plot_2 = out.x2_obj8;
 data = var_plot_2.signals.values;
 time = var_plot_2.time;
 subplot(2,2,2)
@@ -27,25 +27,25 @@ title("x2")
 xlabel("Czas(s)")
 ylabel("Wartość")
 hold on
-var_plot_2_2 = out.x2_obv;
+var_plot_2_2 = out.x2_obv8;
 data = var_plot_2_2.signals.values;
 time = var_plot_2_2.time;
 stairs(time,data,'Color','b','LineStyle','--', 'LineWidth',0.9)
 legend("x2 obiektu","x2 obserwatora","Location", "southeast")
-var_plot_3 = out.x3_obj;
+var_plot_3 = out.x3_obj8;
 data = var_plot_3.signals.values;
 time = var_plot_3.time;
 subplot(2,2,3)
 stairs(time,data,'Color','r','LineStyle','-', 'LineWidth',0.9)
 set(gca,'fontsize', 8)
 hold on
-var_plot_3_2 = out.x3_obv;
+var_plot_3_2 = out.x3_obv8;
 data = var_plot_3_2.signals.values;
 time = var_plot_3_2.time;
 stairs(time,data,'Color','b','LineStyle','--', 'LineWidth',0.9)
 legend("x3 obiektu","x3 obserwatora","Location", "southeast")
 % xticks(0:1:10);
-var_plot_4 = out.uo;
+var_plot_4 = out.uo8;
 data = var_plot_4.signals.values;
 time = var_plot_4.time;
 set(gca,'fontsize', 8)
@@ -63,4 +63,4 @@ hold off
 title("U")
 % legend("x1", "x2","x3","u")
 % ylim([-2 3])
-print('zad7_001.png','-dpng','-r400')
+print('zad8_001.png','-dpng','-r400')
