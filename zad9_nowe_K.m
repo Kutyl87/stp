63c8d0c9a605f229c71a44a9fb1dd09621afc427
pkutyl <01168894@pw.edu.pl>
2023-04-27 22:53:57 +0200

    gd

--- zad9_nowe_K.m
+++ zad9_nowe_K.m
@@ -1,6 +1,6 @@
 format long;
- A_var_2 = [-Gz.Denominator{1}(2) -Gz.Denominator{1}(3) -Gz.Denominator{1}(4); 1 0 0; 0 1 0]'
- B_var_2 = [Gz.Numerator{1}(2) Gz.Numerator{1}(3) Gz.Numerator{1}(4)];
- C_var2 = [1;0;0]';
+ A_var_2 = [-Gz.Denominator{1}(2) -Gz.Denominator{1}(3) -Gz.Denominator{1}(4); 1 0 0; 0 1 0]';
+ B_var_2 = [Gz.Numerator{1}(2) Gz.Numerator{1}(3) Gz.Numerator{1}(4)]';
+ C_var2 = [1 0 0];
  bieguny;
- Kvar_2 = acker(A,B,bieguny);
+ Kvar_2 = acker(A_var_2,B_var_2,bieguny)
